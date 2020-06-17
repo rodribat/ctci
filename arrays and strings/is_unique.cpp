060b727d6eda1b44e9693d43ce3aef140194db49
Rodrigo A. Batista <rodribat@gmail.com>
2020-06-17 00:46:32 -0300

    Is Unique

--- arrays and strings/is_unique.cpp
+++ arrays and strings/is_unique.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-// using additional data structure
+// using additional data structure - O(n)
 bool isUnique(string w)
 {
     set<char> s = set<char>();
@@ -19,7 +19,7 @@ bool isUnique(string w)
     return true;
 }
 
-// without additional data structure
+// without additional data structure - O(n²)
 bool isUnique2(string w)
 {
     for (int i=0; i<w.size(); ++i)
@@ -30,12 +30,32 @@ bool isUnique2(string w)
     return true;
 }
 
+// using an array as char set - O(n) or O(min(c, n)) assuming the character set is fixed
+bool isUnique3(string w)
+{
+    if (w.size() > 256) return false;
+
+    bool char_set[256];
+    for (int i=0; i<256; ++i) char_set[i] = false;
+
+    for (int i=0; i<w.size(); ++i)
+    {
+        char val = w[i];
+        if (char_set[val])
+            return false; // already found this char in string
+
+        char_set[val] = true;
+    }
+
+    return true;
+}
+
 main()
 {
     string s;
     cin >> s;
 
-    if (isUnique2(s))
+    if (isUnique3(s))
         cout << "IS UNIQUE" << endl;
     else
         cout << "NOT UNIQUE" << endl;
