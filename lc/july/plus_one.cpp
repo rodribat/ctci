0ee6975d0cab2599689bff8d78697f266496df7a
Rodrigo A. Batista <rodribat@gmail.com>
2020-07-06 23:16:45 -0300

    Plus One

---
+++ lc/july/plus_one.cpp
@@ -0,0 +1,42 @@
+
+/*
+    PlusOne
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry = 1;
+        
+        for (int i=digits.size()-1; i>=0; i--)
+        {
+            int d = digits[i];
+            digits[i] = (d + carry) % 10;
+            carry = (d + carry) / 10;
+        }
+        
+        if (carry)
+            digits.insert(digits.begin(), 1);
+        
+        return digits;
+    }
+};
+
+int main()
+{
+    vector<int> v({9,8,1});
+    Solution s;
+    s.plusOne(v);
+
+    for (int i=0; i<v.size(); ++i)
+        cout << to_string(v[i]) << ", ";
+    
+    cout << endl;
+
+    return 0;
+}
